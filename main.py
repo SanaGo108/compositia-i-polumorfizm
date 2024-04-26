94e5cdb91f9f402d786011edd8e891cb3db0e31c
SanaGo108 <oksana135@mail.ru>
2024-04-26 23:06:55 +0300

    add zadanie2

--- main.py
+++ main.py
@@ -35,7 +35,46 @@ class Cow(Animal):
     def make_sound(self):
         print(f"{self.name} мууу")
 
-# Creating instances of each animal with names and ages
 animals = [Dog("Дружок", 3), Cat("Мурка", 2), Cow("Зорька", 5)]
 for animal in animals:
+    animal.make_sound()
+
+class Bird(Animal):
+    def __init__(self, name, age, wing_span):
+        super().__init__(name, age)
+        self.wing_span = wing_span
+
+    def make_sound(self):
+        """Звук, характерный для птиц."""
+        print(f"{self.name} чирикает.")
+
+class Mammal(Animal):
+    def __init__(self, name, age, fur_color):
+        super().__init__(name, age)
+        self.fur_color = fur_color
+
+    def make_sound(self):
+        """Звук, характерный для млекопитающих."""
+        print(f"{self.name} издает звук.")
+
+class Reptile(Animal):
+    def __init__(self, name, age, scale_type):
+        super().__init__(name, age)
+        self.scale_type = scale_type
+
+    def make_sound(self):
+        """Звук, характерный для рептилий."""
+        print(f"{self.name} шипит.")
+
+eagle = Bird("Орел", 5, 2.0)
+sparrow = Bird("Воробей", 1, 0.1)
+cat = Mammal("Мурка", 2, "серый")
+dog = Mammal("Дружок", 3, "черный")
+cow = Mammal("Зорька", 5, "рыжая")
+snake = Reptile("Каа", 2, "гладкая")
+turtle = Reptile("Тортилла", 12, "хордовая")
+
+animals = [eagle, sparrow, cat, dog, snake, turtle]
+for animal in animals:
+    animal.eat()
     animal.make_sound()
